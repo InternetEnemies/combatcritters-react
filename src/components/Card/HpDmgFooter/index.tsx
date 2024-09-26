f721667ee5f5b69ebd7d3a002d559567d43e9589
Kevin Breckman <kevinbreckman@gmail.com>
2024-09-26 00:15:33 -0500

    Add icons to hp and damage

--- src/components/Card/HpDmgFooter/index.tsx
+++ src/components/Card/HpDmgFooter/index.tsx
@@ -3,14 +3,18 @@ import './hpDmgFooter.css'
 interface Props {
     hp: number;
     damage: number;
-    backgroundColor: string;
+    rarity: string;
   }
   
-  const HpDmgFooter: React.FC<Props> = ({ hp, damage, backgroundColor }) => {
+  const HpDmgFooter: React.FC<Props> = ({ hp, damage, rarity }) => {
     return (
-        <div className="footerRoot" style={{backgroundColor}}>
-          <span className="hp"> {hp} </span>
-          <span className="damage"> {damage} </span>
+        <div className={`footerRoot ${rarity}`}>
+          <div className="hp">
+            <img className="hpImage" src="/assets/images/heart.svg"></img>
+            <span className="hpText"> {hp} </span></div>
+          <div className="damage">
+            <img className="swordImage" src="/assets/images/sword.svg"></img>
+            <span className="damageText"> {damage} </span></div>
         </div>
       );
     };
