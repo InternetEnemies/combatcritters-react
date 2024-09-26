d3cd98ee910065b57f9910e80ab7de59913f2027
Kevin Breckman <kevinbreckman@gmail.com>
2024-09-26 14:51:13 -0500

    Create Inventory Component

---
+++ src/pages/Collection/Inventory/index.tsx
@@ -0,0 +1,13 @@
+import './inventory.css'
+  
+const Inventory: React.FC = () => {
+    return (
+        <div className="inventoryRoot">
+    
+        </div>
+    );
+};
+
+
+export default Inventory;
+  
