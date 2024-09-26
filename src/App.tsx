e74fa8da651a7cf837acbd464f84f97e3455e69f
Kevin Breckman <kevinbreckman@gmail.com>
2024-09-26 00:16:20 -0500

    Add test card

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import Card from 'components/Card';
+
 function App() {
   return (
     <div className="App">
-
+      <Card 
+        rarity="epic"
+        name="UglyMan, the Hideous Hero" 
+        playCost={69}
+        imagePath="assets/images/cardImage.jpeg" 
+        abilities={[1, 2, 3]}
+        type="critter"
+        description="Uglyman, once shunned for his grotesque appearance, now defends the weak with unmatched strength, proving beauty isn’t everything." 
+        hp={35}
+        damage={10}
+      />
     </div>
   );
 }
