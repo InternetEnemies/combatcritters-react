5786b4ef01adeb708f8c5eb24383c17f490a7daa
Kevin Breckman <kevinbreckman@gmail.com>
2024-09-26 10:57:36 -0500

    Start abillity count at 0

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ function App() {
   return (
     <div className="App">
       <Card 
-        rarity="epic"
+        rarity="common"
         name="UglyMan, the Hideous Hero" 
         playCost={69}
         imagePath="assets/images/cardImage.jpeg" 
-        abilities={[1, 2, 3]}
-        type="critter"
+        abilities={[0, 1, 2]}
+        type="item"
         description="Uglyman, once shunned for his grotesque appearance, now defends the weak with unmatched strength, proving beauty isn’t everything." 
         hp={35}
         damage={10}
