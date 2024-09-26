b2794c15de64dc429ef1fa8e337158d7609fe2f7
Kevin Breckman <kevinbreckman@gmail.com>
2024-09-26 09:26:29 -0500

    Make component invisible if the type is item

--- src/components/Card/HpDmgFooter/index.tsx
+++ src/components/Card/HpDmgFooter/index.tsx
@@ -4,20 +4,26 @@ interface Props {
     hp: number;
     damage: number;
     rarity: string;
+    type: string
   }
   
-  const HpDmgFooter: React.FC<Props> = ({ hp, damage, rarity }) => {
+  const HpDmgFooter: React.FC<Props> = ({ hp, damage, rarity, type }) => {
+    const visibilityClass = type === "item" ? "invisible" : "";
+  
     return (
-        <div className={`footerRoot ${rarity}`}>
-          <div className="hp">
-            <img className="hpImage" src="/assets/images/heart.svg"></img>
-            <span className="hpText"> {hp} </span></div>
-          <div className="damage">
-            <img className="swordImage" src="/assets/images/sword.svg"></img>
-            <span className="damageText"> {damage} </span></div>
+      <div className={`footerRoot ${rarity} ${visibilityClass}`}>
+        <div className="hp">
+          <img className="hpImage" src="/assets/images/heart.svg" alt="Heart" />
+          <span className="hpText"> {hp} </span>
         </div>
-      );
-    };
+        <div className="damage">
+          <img className="swordImage" src="/assets/images/sword.svg" alt="Sword" />
+          <span className="damageText"> {damage} </span>
+        </div>
+      </div>
+    );
+  };
+  
   
   export default HpDmgFooter;
   
