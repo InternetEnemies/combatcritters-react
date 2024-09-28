c749c763930a84faafe4690e17a9cd2e89f34ded
Kevin Breckman <kevinbreckman@gmail.com>
2024-09-28 11:05:28 -0500

    Add cardCount to Card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,40 +1,53 @@
-import Header from './Header'
-import CardImage from './CardImage'
-import HpDmgFooter from './HpDmgFooter'
-import Abilities from './Abilities'
-import Description from './Description'
-import PlayCost from './PlayCost'
-import Type from './Type'
-import './card.css'
+import Header from "./Header";
+import CardImage from "./CardImage";
+import HpDmgFooter from "./HpDmgFooter";
+import Abilities from "./Abilities";
+import Description from "./Description";
+import PlayCost from "./PlayCost";
+import Type from "./Type";
+import "./card.css";
+import CardCount from "./CardCount";
 
 interface CardProps {
-    id: number;
-    rarity: string;
-    name: string;
-    playCost: number;
-    imagePath: string;
-    abilities: number[];
-    type: string;
-    description: string;
-    hp: number;
-    damage: number;
-  }
-  
-  const Card: React.FC<CardProps> = ({ rarity, name, playCost, imagePath, abilities, type, description, hp, damage }) => {
-    return (
-      <div className={`cardRoot ${rarity}`}>
-        <Header name={name} rarity={rarity}/>
-        <div className={`cardInner ${rarity}`}>
-          <CardImage imagePath={imagePath}/>
-          {type === "critter" && <Abilities abilities={abilities}/>}
-          <Description description={description} type={type} />
-        </div>
-        <HpDmgFooter hp={hp} damage={damage} rarity={rarity} type={type}/>
-        <PlayCost playCost={playCost}/>
-        <Type type={type}/>
+  id: number;
+  rarity: string;
+  name: string;
+  playCost: number;
+  imagePath: string;
+  abilities: number[];
+  type: string;
+  description: string;
+  hp: number;
+  damage: number;
+  cardCount?: number;
+}
+
+const Card: React.FC<CardProps> = ({
+  rarity,
+  name,
+  playCost,
+  imagePath,
+  abilities,
+  type,
+  description,
+  hp,
+  damage,
+  cardCount,
+}) => {
+  return (
+    <div className={`cardRoot ${rarity}`}>
+      <Header name={name} rarity={rarity} />
+      <div className={`cardInner ${rarity}`}>
+        <CardImage imagePath={imagePath} />
+        {type === "critter" && <Abilities abilities={abilities} />}
+        <Description description={description} type={type} />
       </div>
-    );
-  };
-  
-  
-  export default Card;  
+      <HpDmgFooter hp={hp} damage={damage} rarity={rarity} type={type} />
+      <PlayCost playCost={playCost} />
+      <Type type={type} />
+      {cardCount !== undefined && <CardCount amount={cardCount}/>}
+    </div>
+  );
+};
+
+export default Card;
