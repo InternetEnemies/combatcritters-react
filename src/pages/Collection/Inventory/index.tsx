e19d3ea36748e68f39646831cc13d28acd2f19d4
Kevin Breckman <kevinbreckman@gmail.com>
2024-09-29 12:48:59 -0500

    Add cards to inventory

--- src/pages/Collection/Inventory/index.tsx
+++ src/pages/Collection/Inventory/index.tsx
@@ -1,13 +1,47 @@
-import './inventory.css'
-  
-const Inventory: React.FC = () => {
-    return (
-        <div className="inventoryRoot">
-    
-        </div>
-    );
+import { getCards } from "testingCode";
+import React, { useEffect } from "react";
+import { DndContext } from "@dnd-kit/core";
+import DraggableCard from "components/DraggableCard";
+import "./inventory.css";
+import {Card} from "card"
+
+interface InventoryProps {
+  selectedCards: Card[];
+  setSelectedCards: (cards: Card[]) => void;
+}
+
+const Inventory: React.FC<InventoryProps> = ({
+  selectedCards,
+  setSelectedCards,
+}) => {
+  useEffect(() => {
+    const cards = getCards(); 
+    setSelectedCards(cards);
+  }, [setSelectedCards]);
+
+  return (
+    <div className="inventoryRoot">
+      <div className="cardGrid">
+        {selectedCards.map((card) => (
+          <DraggableCard
+            key={card.id}
+            id={card.id}
+            rarity={card.rarity}
+            name={card.name}
+            playCost={card.playCost}
+            imagePath={card.imagePath}
+            abilities={card.abilities}
+            type={card.type}
+            description={card.description}
+            hp={card.hp}
+            damage={card.damage}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
-
 export default Inventory;
-  
+
+
