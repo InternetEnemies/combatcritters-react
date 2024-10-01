e75645812c7bac4dd1624e66060f7b7d422c282b
jackal <v@njackal.com>
2024-10-01 13:41:30 -0500

    added --isolated-modules workaround for test

--- src/test/dummy.test.ts
+++ src/test/dummy.test.ts
@@ -1,4 +1,5 @@
 //this file is here to keep the CI happy when there are no tests, once tests are added this should be removed
+import "react" // this is just so the isolated modules doesn't complain
 
 it("should work", () => {
     expect(true).toBe(true);
