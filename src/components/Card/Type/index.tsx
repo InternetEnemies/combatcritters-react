171eb8d61d407c5c7528c914c5908cbe3a50f2e2
Kevin Breckman <kevinbreckman@gmail.com>
2024-10-05 10:38:30 -0500

    Refactor card to work with visitor

--- src/components/Card/Type/index.tsx
+++ src/components/Card/Type/index.tsx
@@ -1,25 +1,28 @@
 import './type.css'
+import { ICard, ICardCritter, ICardItem } from 'combatcritters-ts/src/objects';
+import { ICardVisitor } from 'combatcritters-ts/src/ICardVisitor';
 
 interface Props {
     type: string;
   }
   
-  const Type: React.FC<Props> = ({ type }) => {
-    const getImagePath = (type: string) => {
-      if(type === "critter") {
-        return "/assets/images/critter.svg";
-      }
-      else {
-        return "/assets/images/item.svg";
-      }
+  const Type: React.FC<{ card: ICard }> = ({ card }) => {
+    let imagePath;
+
+    const visitor: ICardVisitor = {
+      visitCritter: (critter: ICardCritter): void => {
+        imagePath = "/assets/images/critter.svg";
+      },
+      visitItem: (item: ICardItem): void => {
+        imagePath = "/assets/images/item.svg";
+      },
     };
-  
-    return (
-      <img
-        className="typeImage"
-        src={getImagePath(type)}
-      />
-    );
+
+    card.accept(visitor);
+
+
+
+    return <img className="typeImage" src={imagePath} />;
   };
   
   export default Type;
