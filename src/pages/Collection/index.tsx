1df1b95943f4d46786d55716760085b0f2d28d5f
Kevin Breckman <kevinbreckman@gmail.com>
2024-10-05 10:45:32 -0500

    Add inventory and deck components to Collection

--- src/pages/Collection/index.tsx
+++ src/pages/Collection/index.tsx
@@ -1,15 +1,79 @@
-import './collection.css'
-import NavBar from 'components/NavBar';
-  
+import React, { useState, useEffect } from "react";
+import { DndContext, DragOverlay } from "@dnd-kit/core";
+import { SortableContext } from "@dnd-kit/sortable";
+import Inventory from "./Inventory";
+import Decks from "./Decks";
+import { ISortableDeck } from "interfaces/ISortableDeck";
+import NavBar from "components/NavBar";
+import "./collection.css";
+import { ISortableCard } from "interfaces/ISortableCard";
+import SortableCard from "components/SortableCard";
+import { useDragAndDrop } from "hooks/useDragAndDrop";
+import DeckManager from "api/DeckManager";
+import { convertToSortableDeck } from "utils/deckUtils";
+
 const Collection: React.FC = () => {
-    return (
-        <div className="collectionRoot">
-            <NavBar/>
-            <div className="cardsDecksContainer"></div>
+  
+  const [selectedCards, setSelectedCards] = useState<ISortableCard[]>([]);
+  const [localDeck, setLocalDeck] = useState<ISortableDeck>({
+    id: 0,
+    name: "No Deck Selected",
+    cards: [],
+  });
+
+  useEffect(() => {
+    const initializeDeck = async () => {
+      const decks = await DeckManager.getInstance().getDecks();
+      if(decks.length > 0) {
+        setLocalDeck(convertToSortableDeck(decks[0])); 
+    }
+      
+    };
+    initializeDeck();
+  }, []); 
+
+  const {
+    activeCard,
+    highlightDeck,
+    handleDragStart,
+    handleDragEnd,
+    handleDragOver,
+  } = useDragAndDrop(selectedCards, localDeck, setLocalDeck);
+
+  return (
+    <DndContext
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+      onDragMove={handleDragOver}
+    >
+      <div className="collectionRoot">
+        <NavBar />
+        <div className="invDecksContainer">
+          {localDeck && (
+            <SortableContext
+              items={localDeck.cards.map((card) => card.instanceId)}
+            >
+              <Decks
+                localDeck={localDeck}
+                setLocalDeck={setLocalDeck}
+                highlight={highlightDeck}
+              />
+            </SortableContext>
+          )}
+          <Inventory
+            selectedCards={selectedCards}
+            setSelectedCards={setSelectedCards}
+          />
         </div>
-    );
+
+        <DragOverlay>
+          {activeCard ? (
+            <SortableCard sortableCard={activeCard} translucent={true} />
+          ) : null}
+        </DragOverlay>
+      </div>
+    </DndContext>
+  );
 };
 
-
 export default Collection;
-  
