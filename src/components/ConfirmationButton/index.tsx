91db465b84e8646ba4cc40f87cbc19f487d02d46
Kevin Breckman <kevinbreckman@gmail.com>
2024-10-07 22:06:31 -0500

    Change cursor to pointer on hover

--- src/components/ConfirmationButton/index.tsx
+++ src/components/ConfirmationButton/index.tsx
@@ -5,11 +5,12 @@
 
 import React, { useState } from "react";
 import ConfirmationDialog from "components/ConfirmationDialog";
+import "./confirmationButton.css";
 
 interface ConfirmationButtonProps {
-  onClick: () => void;         //Callback to execute when the user confirms. 
+  onClick: () => void; //Callback to execute when the user confirms.
   confirmationMessage: string; //Confirmation message to display in the poopup.
-  child: React.ReactElement;   //Allow custom button components to be passed.
+  child: React.ReactElement; //Allow custom button components to be passed.
 }
 
 const ConfirmationButton: React.FC<ConfirmationButtonProps> = ({
@@ -25,14 +26,17 @@ const ConfirmationButton: React.FC<ConfirmationButtonProps> = ({
   };
 
   const handleConfirm = () => {
+    console.log("here");
     onClick();
     setShowDialog(false);
   };
 
   return (
     <>
-      {React.cloneElement(child, { onClick: handleButtonClick })}
-
+      {React.cloneElement(child, {
+        onClick: handleButtonClick,
+        style: { cursor: "pointer", ...child.props.style }, 
+      })}
       <ConfirmationDialog
         show={showDialog}
         setShowDialog={setShowDialog}
