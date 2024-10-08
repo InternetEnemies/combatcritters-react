59c76de879334025a30689cb8855f7085d0ab312
Kevin Breckman <kevinbreckman@gmail.com>
2024-10-08 15:36:50 -0500

    Add profile icon to navbar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,13 +1,19 @@
-import './navBar.css'
+import React, { useState } from "react";
+import "./navBar.css";
+import ProfileButton from "components/ProfileButton"; 
 
 const NavBar: React.FC = () => {
+  const [hasNotifications, setHasNotifications] = useState(true); 
+  const notificationCount = 3; 
+
   return (
     <div className="navBarRoot">
-      
+      <ProfileButton
+        hasNotifications={hasNotifications}
+        notificationCount={notificationCount}
+      />
     </div>
   );
 };
 
-
 export default NavBar;
-  
