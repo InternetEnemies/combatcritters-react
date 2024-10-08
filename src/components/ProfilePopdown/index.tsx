753853ce93774b1020b0a064a8a467403e25dd72
Kevin Breckman <kevinbreckman@gmail.com>
2024-10-08 15:37:28 -0500

    Create ProfilePopdown

---
+++ src/components/ProfilePopdown/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./profilePopdown.css";
+import { useNavigate } from "react-router-dom";
+import logoutIcon from "assets/icons/logout.svg"; 
+
+interface PopdownProps {
+  isVisible: boolean;
+}
+
+const ProfilePopdown: React.FC<PopdownProps> = ({ isVisible }) => {
+  const navigate = useNavigate();
+
+  const handleProfileClick = () => {
+   //todo navigate("/profile");
+  };
+
+  const handleLogoutClick = () => {
+    navigate("/login");
+  };
+
+  if (!isVisible) {
+    return null;
+  }
+
+  return (
+    <div className="profilePopdownRoot">
+      <button className="popdownButton" onClick={handleProfileClick}>
+        Your Profile
+      </button>
+      <button className="popdownButton" onClick={handleLogoutClick}>
+        <img src={logoutIcon} alt="Logout Icon" className="logoutIcon" />{" "}
+        Logout
+      </button>
+    </div>
+  );
+};
+
+export default ProfilePopdown;
