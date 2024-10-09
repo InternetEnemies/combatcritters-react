e580ad3cc2838da0bcb09fafd1be11600809cc75
Kevin Breckman <kevinbreckman@gmail.com>
2024-10-09 15:21:01 -0500

    Create useFriendsList

---
+++ src/hooks/useFriendsList.ts
@@ -0,0 +1,77 @@
+/**
+ * @Created 2024-10-07
+ * @Brief Hook that fetches a user's list of friends.
+ */
+
+import { ClientSingleton } from "ClientSingleton";
+import { IUser } from "combatcritters-ts";
+import { useEffect, useState } from "react";
+import { IDeck } from "combatcritters-ts";
+
+export const useFriendsList = () => {
+  const [friends, setFriends] = useState<IUser[]>([]);
+  const [selectedFriend, setSelectedFriend] = useState<IUser | null>(null);
+  const [showDeck, setShowDeck] = useState(false);
+  const [friendsDeck, setFriendsDeck] = useState<IDeck | null>(null);
+
+  useEffect(() => {
+    const fetchFriends = async () => {
+      try {
+        const f = await ClientSingleton.getInstance().user.friends.getFriends();
+        setFriends(f);
+      } catch (error) {
+        console.error("Error fetching friends:", error);
+      }
+    };
+
+    fetchFriends();
+  }, []);
+
+
+  const onFriendClick = async (user: IUser) => {
+    setSelectedFriend(user);
+
+    try {
+      const deck = await ClientSingleton.getInstance().user.profile.getDeck();
+      setFriendsDeck(deck);
+    } catch (error) {
+      console.error("Error fetching friend's deck:" + error);
+    }
+    setShowDeck(true);
+  };
+
+  return { friends, selectedFriend, showDeck, setShowDeck, onFriendClick };
+};
+
+// import { getUsers, ITestUser } from "ITestUser";
+// import { ClientSingleton } from "ClientSingleton";
+// import { IUser } from "combatcritters-ts";
+// import { useEffect, useState } from "react";
+// import { IDeck } from "combatcritters-ts";
+
+// export const useFriendsList = () => {
+//   const [friends, setFriends] = useState<ITestUser[]>([]);
+//   const [selectedFriend, setSelectedFriend] = useState<ITestUser | null>(null);
+//   const [showDeck, setShowDeck] = useState(false);
+
+//   useEffect(() => {
+//     const fetchFriends = async () => {
+//       try {
+//         const f = await getUsers();
+//         setFriends(f);
+//       } catch (error) {
+//         console.error("Error fetching friends:", error);
+//       }
+//     };
+
+//     fetchFriends();
+//   }, []);
+
+//   const onFriendClick = async (user: ITestUser) => {
+//     setSelectedFriend(user);
+
+//     setShowDeck(true);
+//   };
+
+//   return { friends, selectedFriend, showDeck, setShowDeck, onFriendClick };
+// };
