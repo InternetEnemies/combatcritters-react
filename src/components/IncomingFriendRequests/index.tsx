121b684c350cb473595eb098959e325db5892c1f
Kevin Breckman <kevinbreckman@gmail.com>
2024-10-09 15:18:33 -0500

    Create IncomingFriendRequests componeent

---
+++ src/components/IncomingFriendRequests/index.tsx
@@ -0,0 +1,43 @@
+/**
+ * @Created 2024-10-07
+ * @Brief Component for accepting incoming friend requests.
+ */
+
+import React from "react";
+import "./incomingFriendRequests.css";
+import { useManageFriendRequests } from "hooks/useManageFriendRequests";
+import checkmark from "assets/icons/checkmark.svg";
+import cross from "assets/icons/cross.svg";
+
+const IncomingFriendRequests: React.FC = () => {
+  const { friendRequests, acceptFriendRequest } =
+    useManageFriendRequests();
+
+  return (
+    <div className="friendRequestsRoot">
+      <h3>Incoming Friend Requests</h3>
+      {friendRequests.length > 0 ? (
+        <ul className="friendRequestsList">
+          {friendRequests.map((user) => (
+            <li key={user.id} className="friendRequestItem">
+              <span>{user.username}</span>
+              <div className="actionButtons">
+                <button
+                  className="iconButton acceptButton"
+                  onClick={() => acceptFriendRequest(user)}
+                >
+                  <img src={checkmark} alt="Accept" className="acceptIcon" />
+                </button>
+                
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No incoming friend requests</p>
+      )}
+    </div>
+  );
+};
+
+export default IncomingFriendRequests;
