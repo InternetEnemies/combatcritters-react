511491c2adbf83d0929dd79cb1d5f8a8873faab7
Kevin Breckman <kevinbreckman@gmail.com>
2024-10-08 19:29:43 -0500

    Add route to profile

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,16 @@ import {
 } from "react-router-dom";
 import Collection from "pages/Collection";
 import Login from "pages/Login";
+import Profile from "pages/Profile";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/" element={<Navigate to="/profile" />} />
         <Route path="/collection" element={<Collection />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile/>}/>
       </Routes>
     </Router>
   );
