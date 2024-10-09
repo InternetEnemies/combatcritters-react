d5fb1ea1efdc04751a0b81833d398e7e6e6e0403
Kevin Breckman <kevinbreckman@gmail.com>
2024-10-08 22:28:17 -0500

    Use FeatureDeck component

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,53 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import NavBar from "components/NavBar";
 import "./profile.css";
-
-interface Deck {
-  id: number;
-  name: string;
-}
-
-const decks: Deck[] = [
-  { id: 1, name: "Deck 1" },
-  { id: 2, name: "Deck 2" },
-  { id: 3, name: "Deck 3" },
-];
+import FeatureDeck from "components/FeatureDeck";
 
 const Profile: React.FC = () => {
-  const [selectedDeck, setSelectedDeck] = useState<Deck | null>(null);
-
-  const handleDeckClick = (deck: Deck) => {
-    setSelectedDeck(deck);
-  };
 
   return (
     <div className="profileRoot">
       <NavBar />
       <div className="profileContainer">
-        <div className="deckContainer">
-          <div className="deckListContainer">
-            <h3>Your Decks</h3>
-            <ul className="deckList">
-              {decks.map((deck) => (
-                <li
-                  key={deck.id}
-                  className={`deckItem ${
-                    selectedDeck?.id === deck.id ? "active" : ""
-                  }`}
-                  onClick={() => handleDeckClick(deck)}
-                >
-                  {deck.name}
-                </li>
-              ))}
-            </ul>
-          </div>
-
-          <div className="deckContentContainer">
-            <h3>{selectedDeck ? selectedDeck.name : "Select a deck"}</h3>
-            {selectedDeck && <p>Displaying cards for {selectedDeck.name}</p>}
-          </div>
+        <FeatureDeck/>
         </div>
-      </div>
     </div>
   );
 };
