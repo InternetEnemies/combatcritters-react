aa25c26bc7c571a874c515a42120bdafb6b4a22b
Kevin Breckman <kevinbreckman@gmail.com>
2024-10-09 15:21:37 -0500

    Create useManageFriendRequests

---
+++ src/hooks/useManageFriendRequests.ts
@@ -0,0 +1,40 @@
+/**
+ * @Created 2024-10-07
+ * @Brief Hook for accepting friend requests.
+ */
+
+import { ClientSingleton } from "ClientSingleton";
+import { Client, IUser } from "combatcritters-ts";
+import { useEffect, useState } from "react";
+
+export const useManageFriendRequests = () => {
+  const [friendRequests, setFriendRequests] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    const fetchFriendRequests = async () => {
+      try {
+        const fRequests =
+          await ClientSingleton.getInstance().user.friends.getFriendsRequests();
+        setFriendRequests(fRequests);
+      } catch (error) {
+        console.error("Error fetching friend requests:", error);
+      }
+    };
+
+    fetchFriendRequests();
+  }, []);
+
+  const acceptFriendRequest = async (user: IUser) => {
+    try {
+      await ClientSingleton.getInstance().user.friends.addFriend(user);
+      setFriendRequests(
+        (prevRequests) =>
+          prevRequests.filter((request) => request.id !== user.id)
+      );
+    } catch (error) {
+      console.error("Error accepting friend request:", error);
+    }
+  };
+
+  return { friendRequests, acceptFriendRequest };
+};
