29ee5353c1ac996035abc407e7537748d5f6eb54
Kevin Breckman <kevinbreckman@gmail.com>
2024-10-08 22:33:38 -0500

    useNavigate

--- src/components/ProfilePopdown/index.tsx
+++ src/components/ProfilePopdown/index.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 import "./profilePopdown.css";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
 
 interface PopdownProps {
   isVisible: boolean;
 }
 
 const ProfilePopdown: React.FC<PopdownProps> = ({ isVisible }) => {
+  const navigate = useNavigate();
 
   const handleProfileClick = () => {
    navigate("/profile");
-  };
-
-  const handleLogoutClick = () => {
-    navigate("/login");
   };
 
   if (!isVisible) {
