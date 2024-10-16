50db23a701d7eadfa138af55a57c295f964f0427
Kevin Breckman <kevinbreckman@gmail.com>
2024-10-16 12:29:18 -0500

    Add toast message on friend request send

--- src/pages/Profile/components/SendFriendRequest/index.tsx
+++ src/pages/Profile/components/SendFriendRequest/index.tsx
@@ -7,13 +7,17 @@ import React, { useState } from "react";
 import "./sendFriendRequest.css";
 import { ClientSingleton } from "ClientSingleton";
 import sendButton from "assets/icons/send.svg";
+import { useToast } from "hooks/useToast";
+import Toast from "components/Toast";
 
 const SendFriendRequest: React.FC = () => {
   const [username, setUsername] = useState<string>("");
+  const { showToast, setShowToast, triggerToast, toastMessage } = useToast();
 
   const handleSendRequest = () => {
     try {
       ClientSingleton.getInstance().user.friends.addFriend(username);
+      triggerToast("Friend Request Sent!");
     } catch (error) {
       console.error("Error sending friend request:" + error);
     }
@@ -28,7 +32,13 @@ const SendFriendRequest: React.FC = () => {
         placeholder="Send Friend Request"
         className="friendRequestInput"
       />
-      <img className="sendButton" src={sendButton} alt="Send Button" onClick={handleSendRequest}/>
+      <img
+        className="sendButton"
+        src={sendButton}
+        alt="Send Button"
+        onClick={handleSendRequest}
+      />
+      <Toast show={showToast} setShow={setShowToast} message={toastMessage} />
     </div>
   );
 };
