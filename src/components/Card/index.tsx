9d5646e0dbdcf08f4598e47031601024c80ada6c
Kevin Breckman <kevinbreckman@gmail.com>
2024-10-18 20:05:33 -0500

    Add style prop to Card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,8 +1,4 @@
-/**
- * @Created 2024-10-07
- * @Brief The Card view that is used throughout the application.
- */
-
+import React from "react";
 import Header from "./Header";
 import CardImage from "./CardImage";
 import HpDmgFooter from "./HpDmgFooter";
@@ -18,11 +14,12 @@ import { CardRarity } from "combatcritters-ts/src/objects";
 interface CardProps {
   card: ICard;
   cardCount?: number;
+  style?: React.CSSProperties;
 }
 
-const Card: React.FC<CardProps> = ({ card, cardCount }) => {
+const Card: React.FC<CardProps> = ({ card, cardCount, style }) => {
   return (
-    <div className={`cardRoot ${CardRarity[card.rarity]}`}>
+    <div className={`cardRoot ${CardRarity[card.rarity]}`} style={style}>
       <Header card={card} />
       <div className={`cardInner ${CardRarity[card.rarity]}`}>
         <CardImage imagePath={"/assets/images/cardImage.jpeg"} />
