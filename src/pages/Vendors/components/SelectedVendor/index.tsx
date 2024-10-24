705810c7dfd12b4758cda794870ab37f876f33fd
Kevin Breckman <kevinbreckman@gmail.com>
2024-10-24 17:01:15 -0500

    Add components

--- src/pages/Vendors/components/SelectedVendor/index.tsx
+++ src/pages/Vendors/components/SelectedVendor/index.tsx
@@ -5,7 +5,9 @@
 
 import { IOffer, IVendor } from "combatcritters-ts";
 import "./selectedVendor.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import OffersGrid from "../OffersGrid";
+import SelectedOffer from "../SelectedOffer";
 
 interface SelectedVendorProps {
   isVisible: boolean;
@@ -13,20 +15,51 @@ interface SelectedVendorProps {
   setSelectedVendor: (vendor: IVendor | null) => void;
 }
 
-const SelectedVendor: React.FC<SelectedVendorProps> = ({isVisible, selectedVendor, setSelectedVendor}) => {
+const SelectedVendor: React.FC<SelectedVendorProps> = ({
+  isVisible,
+  selectedVendor,
+  setSelectedVendor,
+}) => {
   const [offers, setOffers] = useState<IOffer[]>([]);
+  const [selectedOffer, setSelectedOffer] = useState<IOffer | null>(null);
+
+  useEffect(() => {
+    const fetchOffers = async () => {
+      if (selectedVendor) {
+        const offers = await selectedVendor.getOffers();
+        setOffers(offers);
+      } else {
+        setOffers([]);
+      }
+    };
+
+    fetchOffers();
+  }, [selectedVendor]); 
 
   const handleButtonClick = () => {
     setSelectedVendor(null);
-  }
-  
-  if(!isVisible) {
+  };
+
+  if (!isVisible) {
     return null;
   }
 
   return (
     <div className="selectedVendorRoot">
-      <button onClick={handleButtonClick} className="vendorSelectionButton">Vendor Selection</button>
+      <button onClick={handleButtonClick} className="vendorSelectionButton">
+        Vendor Selection
+      </button>
+      <div className="vendorInfoAndOffersContainer">
+        <div className="offersGridWrapper">
+          <OffersGrid offers={offers} setSelectedOffer={setSelectedOffer} />
+        </div>
+      </div>
+      <div className="selectedOfferWrapper">
+        <SelectedOffer
+          offer={selectedOffer}
+          vendorName={selectedVendor ? selectedVendor.name : "Vendor"}
+        />
+      </div>
     </div>
   );
 };
