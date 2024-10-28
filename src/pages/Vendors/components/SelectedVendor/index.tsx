5b51235a458a65bd12088f3e064f0d40e877ad6b
Kevin Breckman <kevinbreckman@gmail.com>
2024-10-28 11:07:49 -0500

    Add vendor view

--- src/pages/Vendors/components/SelectedVendor/index.tsx
+++ src/pages/Vendors/components/SelectedVendor/index.tsx
@@ -1,13 +1,20 @@
 /**
  * @Created 2024-10-22
- * @Brief Component for the selected vendor.
+ * @Brief Separate page for the selected vendor.
  */
 
-import { IOffer, IVendor } from "combatcritters-ts";
+import {
+  IDiscountOffer,
+  IOffer,
+  ISpecialOffer,
+  IVendor,
+  IVendorReputation,
+} from "combatcritters-ts";
 import "./selectedVendor.css";
 import { useEffect, useState } from "react";
 import OffersGrid from "../OffersGrid";
 import SelectedOffer from "../SelectedOffer";
+import SelectedVendorView from "../SelectedVendorView";
 
 interface SelectedVendorProps {
   isVisible: boolean;
@@ -21,23 +28,43 @@ const SelectedVendor: React.FC<SelectedVendorProps> = ({
   setSelectedVendor,
 }) => {
   const [offers, setOffers] = useState<IOffer[]>([]);
+  const [discountOffers, setDiscountOffers] = useState<IDiscountOffer[]>([]);
+  const [specialOffers, setSpecialOffers] = useState<ISpecialOffer[]>([]);
   const [selectedOffer, setSelectedOffer] = useState<IOffer | null>(null);
+  const [vendorReputation, setVendorReputation] = useState<IVendorReputation>();
 
+  /*
+    On vendor select, fetch all the different types of offers from the selectedVendor.
+  */
   useEffect(() => {
     const fetchOffers = async () => {
       if (selectedVendor) {
         const offers = await selectedVendor.getOffers();
+        const specialOffers = await selectedVendor.getSpecialOffers();
+        const discountOffers = await selectedVendor.discountOffers();
         setOffers(offers);
+        setSpecialOffers(specialOffers);
+        setDiscountOffers(discountOffers);
+        setVendorReputation(selectedVendor.reputation);
       } else {
         setOffers([]);
       }
     };
-
+    setSelectedOffer(null); //There shouldn't be any selectedOffer when a new vendor has been selected.
     fetchOffers();
-  }, [selectedVendor]); 
+  }, [selectedVendor]);
 
   const handleButtonClick = () => {
     setSelectedVendor(null);
+  };
+
+  /**
+   * Re-render vendorReputation
+   */
+  const refreshVendorReputation = () => {
+    if (selectedVendor) {
+      setVendorReputation(selectedVendor.reputation);
+    }
   };
 
   if (!isVisible) {
@@ -49,15 +76,30 @@ const SelectedVendor: React.FC<SelectedVendorProps> = ({
       <button onClick={handleButtonClick} className="vendorSelectionButton">
         Vendor Selection
       </button>
-      <div className="vendorInfoAndOffersContainer">
+      <div className="vendorViewAndOffersContainer">
+        <div className="vendorViewWrapper">
+          {selectedVendor && vendorReputation ? (
+            <SelectedVendorView
+              vendor={selectedVendor}
+              vendorReputation={vendorReputation}
+            />
+          ) : null}
+        </div>
+        <hr className="separator" style={{ alignSelf: "center" }}></hr>
         <div className="offersGridWrapper">
-          <OffersGrid offers={offers} setSelectedOffer={setSelectedOffer} />
+          <OffersGrid
+            discountOffers={discountOffers}
+            specialOffers={specialOffers}
+            offers={offers}
+            setSelectedOffer={setSelectedOffer}
+          />
         </div>
       </div>
       <div className="selectedOfferWrapper">
         <SelectedOffer
           offer={selectedOffer}
           vendorName={selectedVendor ? selectedVendor.name : "Vendor"}
+          refreshVendorReputation={refreshVendorReputation}
         />
       </div>
     </div>
