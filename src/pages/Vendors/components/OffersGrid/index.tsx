2f608e4af5f9a599d127fa25be247574a66b11a1
Kevin Breckman <kevinbreckman@gmail.com>
2024-10-28 10:50:10 -0500

    Add special and discount offers

--- src/pages/Vendors/components/OffersGrid/index.tsx
+++ src/pages/Vendors/components/OffersGrid/index.tsx
@@ -3,23 +3,49 @@
  * @Brief Offers grid displaying all the offers a vendor has to offer.
  */
 
-import { IOffer } from "combatcritters-ts";
+import { IDiscountOffer, IOffer, ISpecialOffer } from "combatcritters-ts";
 import Offer from "../Offer";
 import "./offersGrid.css";
+import SpecialOffer from "../SpecialOffer";
+import DiscountOffer from "../DiscountOffer";
 
 interface OffersGridProps {
   offers: IOffer[];
+  specialOffers: ISpecialOffer[];
+  discountOffers: IDiscountOffer[];
   setSelectedOffer: (offer: IOffer) => void;
 }
 
 const OffersGrid: React.FC<OffersGridProps> = ({
   offers,
+  specialOffers,
+  discountOffers,
   setSelectedOffer,
 }) => {
   return (
     <div className="offersGridRoot">
+      {specialOffers.map((specialOffer, index) => {
+        return (
+          <SpecialOffer
+            key={specialOffer.offerID}
+            specialOffer={specialOffer}
+            onClick={setSelectedOffer}
+          />
+        );
+      })}
+      {discountOffers.map((discountOffer, index) => {
+        return (
+          <DiscountOffer
+            key={discountOffer.offerID}
+            discountOffer={discountOffer}
+            onClick={setSelectedOffer}
+          />
+        );
+      })}
       {offers.map((offer, index) => {
-        return <Offer key={index} offer={offer} onClick={setSelectedOffer} />;
+        return (
+          <Offer key={offer.offerID} offer={offer} onClick={setSelectedOffer} />
+        );
       })}
     </div>
   );
