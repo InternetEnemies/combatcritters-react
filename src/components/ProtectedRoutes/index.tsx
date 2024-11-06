89d61efb901b48be1654e48669a1d03e09a05f1f
Kevin Breckman <kevinbreckman@gmail.com>
2024-11-05 18:18:37 -0600

    Fetch currency on load

--- src/components/ProtectedRoutes/index.tsx
+++ src/components/ProtectedRoutes/index.tsx
@@ -1,17 +1,31 @@
 import { Outlet, Navigate } from "react-router-dom";
 import { ClientSingleton } from "ClientSingleton";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import { useCurrency } from "contexts/CurrencyContext";
 
 const ProtectedRoutes = () => {
   const [isAuth, setAuth] = useState(false);
-  let [isReady, setReady] = useState(false);
+  const [isReady, setReady] = useState(false);
+  const { updateCurrency } = useCurrency();
+
   useEffect(() => {
-    const updateAuth = async () => {
-      setAuth(await ClientSingleton.getInstance().isLoggedIn());
+    const updateAuthAndCurrency = async () => {
+      const authStatus = await ClientSingleton.getInstance().isLoggedIn();
+      setAuth(authStatus);
+
+      if (authStatus) {
+        try {
+          await updateCurrency();
+        } catch (error) {
+          console.error("Error updating currency:", error);
+        }
+      }
+
       setReady(true);
-    }
-    updateAuth();
-  }, []);
+    };
+
+    updateAuthAndCurrency();
+  }, [updateCurrency]);
 
   if (isReady) {
     return isAuth ? <Outlet /> : <Navigate to="/login" />;
