0cb2579c831b6a635e2ae9c6d2a16ce6f6a42254
Kevin Breckman <kevinbreckman@gmail.com>
2024-11-26 15:08:10 -0600

    Create ElixirHealthBar

---
+++ src/pages/Battle/components/ElixirHealthBar/index.tsx
@@ -0,0 +1,53 @@
+/**
+ * @Created 2024-11-26
+ * @Brief The health and elixir bar used in battles.
+ */
+
+import "./elixirHealthBar.css";
+
+interface ElixirHealthBarProps {
+  currAmount: number;
+  maxAmount: number;
+  isUsersBar: boolean;
+  isHealth: boolean;
+}
+
+const ElixirHealthBar: React.FC<ElixirHealthBarProps> = ({
+  currAmount,
+  maxAmount,
+  isUsersBar,
+  isHealth,
+}) => {
+  const imagePath = isHealth
+    ? "assets/images/heart.svg"
+    : "assets/images/elixir.svg";
+
+  return (
+    <div
+      className={`elixirHealthBarRoot 
+        ${isHealth ? "health" : "elixir"} 
+        ${!isUsersBar ? "reversed" : ""}`}
+    >
+      <div className="elixirHealthInfo">
+        {currAmount}/{maxAmount}
+      </div>
+      <div className="elixirHealthBar">
+        <div className="segmentsContainer">
+          {Array.from({ length: maxAmount }).map((_, i) => (
+            <div key={i} className="segment"></div>
+          ))}
+        </div>
+
+        <div
+          className="barPercentage"
+          style={{
+            height: `${(currAmount/maxAmount)*100}%`,
+          }}
+        ></div>
+      </div>
+      <img className="elixirHeartImage" src={imagePath} alt="Heart" />
+    </div>
+  );
+};
+
+export default ElixirHealthBar;
