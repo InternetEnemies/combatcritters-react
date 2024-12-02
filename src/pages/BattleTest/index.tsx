eab6316d574f984178bb7edf292b523c5b4e4245
VanJackal <v@njackal.com>
2024-12-01 18:48:52 -0600

    this code is a warcrime and I dont care lol

--- src/pages/BattleTest/index.tsx
+++ src/pages/BattleTest/index.tsx
@@ -1,27 +1,72 @@
-import {IBattleClient} from "combatcritters-ts";
+import {IBattleClient, ICard, ICardState} from "combatcritters-ts";
 import {getBattleClient} from "../../BattleProvider";
 import {IMatchStateObserver} from "combatcritters-ts";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {IBattleStateObserver} from "combatcritters-ts";
 
 export const BattleTest = () => {
     // todo remove this later
-    let bclient:IBattleClient
+    let bclient: Promise<IBattleClient> = getBattleClient();
     const [title, setTitle] = useState("BattleTest");
+    const [isTurn, setIsTurn] = useState(false);
 
     class MatchObserver implements IMatchStateObserver {
         gameFound(opponent: string): void {
             setTitle(`found match with ${opponent}`);
         }
     }
+
+    let battleObsv: IBattleStateObserver = {
+        setPlayerTurn: function (isPlayerTurn: boolean): void {
+            setIsTurn(!isTurn);
+        },
+        setPlayerHealth: function (health: number): void {
+            throw new Error("Function not implemented.");
+        },
+        setEnemyHealth: function (health: number): void {
+            throw new Error("Function not implemented.");
+        },
+        setPlayerEnergy: function (energy: number): void {
+            throw new Error("Function not implemented.");
+        },
+        setEnemyEnergy: function (energy: number): void {
+            throw new Error("Function not implemented.");
+        },
+        setHand: function (cards: ICard[]): void {
+            throw new Error("Function not implemented.");
+        },
+        setDrawPileSize: function (size: number): void {
+            throw new Error("Function not implemented.");
+        },
+        setPlayerBufferCards: function (cardStates: (ICardState | null)[]): void {
+            throw new Error("Function not implemented.");
+        },
+        setEnemyBufferCards: function (cardStates: (ICardState | null)[]): void {
+            throw new Error("Function not implemented.");
+        },
+        setEnemyCards: function (cardStates: (ICardState | null)[]): void {
+            throw new Error("Function not implemented.");
+        },
+        setPlayerCards: function (cardStates: (ICardState | null)[]): void {
+            throw new Error("Function not implemented.");
+        }
+    }
+    bclient.then((b) => {
+        b.setBattleStateObserver(battleObsv)
+    })
     const startmatch = async () => {
-        bclient = await getBattleClient()
-        bclient.matchController.match("pvp")
-        bclient.setMatchStateObserver(new MatchObserver())
+        (await bclient).matchController.match("pvp");
+        (await bclient).setMatchStateObserver(new MatchObserver());
+    }
+    const endTurn = async () => {
+        (await bclient).battleController.endTurn()
     }
     return (
         <div>
             <h1>{title}</h1>
+            <h2>{isTurn? "turn":"notTurn"}</h2>
             <button onClick={startmatch}>Matchmake</button>
+            <button onClick={endTurn}>end turn</button>
         </div>
     )
 }
