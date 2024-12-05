cb8b6eb4dc701cbcb147ebf8c90afd91d9a4d8dd
Kevin Breckman <kevinbreckman@gmail.com>
2024-12-05 12:34:02 -0600

    Add alt

--- src/components/Card/PlayCost/index.tsx
+++ src/components/Card/PlayCost/index.tsx
@@ -7,7 +7,7 @@ interface Props {
   const PlayCost: React.FC<Props> = ({ playCost }) => {
     return (
       <div className="playCostRoot">
-        <img src="assets/images/cardElixir.svg" className="elixirImage"></img>
+        <img src="assets/images/cardElixir.svg" className="elixirImage" alt={"Elixir"}></img>
         <span className="playCost">{playCost}</span>
       </div>
     );
